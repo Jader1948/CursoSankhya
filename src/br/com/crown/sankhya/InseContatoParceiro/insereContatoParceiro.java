b198037935dff0ccd159ac85a4264aa03eb5678b
jader <jader.marcelino1995@gmail.com>
2023-09-15 09:39:02 -0300

    novos modulos

--- src/br/com/crown/sankhya/InseContatoParceiro/insereContatoParceiro.java
+++ src/br/com/crown/sankhya/InseContatoParceiro/insereContatoParceiro.java
@@ -1,11 +1,9 @@
-package br.com.crown.eventoProgramado;
+package br.com.crown.sankhya.InseContatoParceiro;
 
 import java.math.BigDecimal;
 
 import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
 import br.com.sankhya.jape.EntityFacade;
-import br.com.sankhya.jape.PersistenceException;
-import br.com.sankhya.jape.bmp.PersistentLocalEntity;
 import br.com.sankhya.jape.core.JapeSession;
 import br.com.sankhya.jape.core.JapeSession.SessionHandle;
 import br.com.sankhya.jape.event.PersistenceEvent;
@@ -19,77 +17,92 @@ import br.com.sankhya.modelcore.util.EntityFacadeFactory;
  * Author: Jader
  * Tabela: TGFPAR
  * Tipo Rotina: Evento Programado
- * Apliacação: Atualizar Numero do endereço do parceiro.
+ * Apliacação: Cria um Contato para novos parceiros caso seja Pessoa fisica
  */
-public class AtualizaNumeroEnderecoEventoProgramado implements EventoProgramavelJava{
+
+
+
+public class insereContatoParceiro implements EventoProgramavelJava{
 
 	@Override
 	public void afterDelete(PersistenceEvent event) throws Exception {
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void afterInsert(PersistenceEvent event) throws Exception {
+
+		DynamicVO parcVo = (DynamicVO)event.getVo();
 		
+		String nomeParceiro = parcVo.asString("NOMEPARC");
+		
+		BigDecimal codParceiro = parcVo.asBigDecimal("CODPARC");
+		
+		String tipPessoa = parcVo.asString("TIPPESSOA");
+		
+				
+		if("F".equals(tipPessoa)) {
+			
+			insertParceiro(nomeParceiro, codParceiro);
+			
+		}		
+				
 	}
 
 	@Override
 	public void afterUpdate(PersistenceEvent event) throws Exception {
+		// TODO Auto-generated method stub
 		
-		DynamicVO parceiroVO = (DynamicVO)event.getVo();
-		
-		BigDecimal codParceiro = parceiroVO.asBigDecimal("CODPARC");
-		
-		AtualizaNumeroEndereco(codParceiro);
-
 	}
-	
-	
+
+	@Override
+	public void beforeCommit(TransactionContext tranCtx) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
 	@Override
 	public void beforeDelete(PersistenceEvent event) throws Exception {
+		// TODO Auto-generated method stub
 		
-
 	}
 
 	@Override
 	public void beforeInsert(PersistenceEvent event) throws Exception {
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void beforeUpdate(PersistenceEvent event) throws Exception {
-			
-//		DynamicVO parceiroVO = (DynamicVO)event.getVo();
-//		
-//		BigDecimal codParceiro = parceiroVO.asBigDecimal("CODPARC");
-//		
-//		parceiroVO.setProperty("NUMEND", codParceiro.toString());
-//		
-	}
-	
-	@Override
-	public void beforeCommit(TransactionContext tranCtx) throws Exception {
+		// TODO Auto-generated method stub
 		
 	}
 	
-	private void AtualizaNumeroEndereco(BigDecimal codParceiro) throws Exception, PersistenceException {
+	
+	private void insertParceiro(String nomeParceiro, BigDecimal codParc) throws Exception {
+		
 		SessionHandle hnd = null;
+		
 		
 		try {
 			hnd  = JapeSession.open();
 			EntityFacade dwfFacade = EntityFacadeFactory.getDWFFacade();
-			PersistentLocalEntity cidadeEntity = dwfFacade.findEntityByPrimaryKey("Parceiro", new Object[] {codParceiro});
-			EntityVO entityVO = cidadeEntity.getValueObject();
-			DynamicVO parcVO = (DynamicVO) entityVO;
+			EntityVO entityVO = dwfFacade.getDefaultValueObjectInstance("Contato");
+			DynamicVO dynamicVO = (DynamicVO) entityVO;
 			
-			parcVO.setProperty("NUMEND", "123456");
-				
-			cidadeEntity.setValueObject((EntityVO) parcVO);
+			dynamicVO.setProperty("NOMECONTATO", nomeParceiro);
+			dynamicVO.setProperty("CODPARC", codParc);
+			 
+			dwfFacade.createEntity("Contato", entityVO);
 			
+						
 		}finally {
 			
 			JapeSession.close(hnd);
 		}
+		
 	}
-
+	
 }
